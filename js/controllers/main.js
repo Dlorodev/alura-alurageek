ba39074e538b5fdd06d53f36585d2a1b0f428bef
Dlorodev <dlorodev@gmail.com>
2024-06-07 16:46:15 -0500

    feat: add the accion to delete a product

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -5,6 +5,7 @@ const productsGrid = d.querySelector('[data-products]');
 const form = d.querySelector('[data-form]');
 
 
+
 function createCard(id, name, price, img) {
     const card = d.createElement('div');
     card.classList.add('card');
@@ -14,7 +15,7 @@ function createCard(id, name, price, img) {
     <p class="p">${name}</p>
     <div class="price__box">
         <p class="p__price">$ ${price}</p>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-trash3-fill svg__card" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-trash3-fill svg__card" viewBox="0 0 16 16" data-id="${id}">
             <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
         </svg>
     </div>
@@ -22,8 +23,12 @@ function createCard(id, name, price, img) {
 
     productsGrid.appendChild(card);
 
+    addDeleteEventListener(id);
+
     return card;
 };
+
+
 
 const render = async () => {
     try {
@@ -33,6 +38,7 @@ const render = async () => {
                 createCard(product.id, product.name, product.price, product.img)
             )
         });
+
     } catch (error) {
         console.log(error);
     }
@@ -42,10 +48,20 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const name = d.querySelector('[data-name]').value;
-    const price = d.querySelector('[data-price]').value;
+    const price = parseFloat(d.querySelector('[data-price]').value);
     const img = d.querySelector('[data-img]').value;
 
     servicesProducts.createProduct(name, price, img).then((response) => console.log(response)).catch((error) => console.log(error));
 });
+
+function addDeleteEventListener(id) {
+    const deleteButton = d.querySelector(`[data-id="${id}"]`);
+    if (deleteButton) {
+        deleteButton.addEventListener('click', () => {
+            servicesProducts.deleteProduct(id);
+        })
+    }
+}
+
 
 render();
--- js/services/product-services.js
+++ js/services/product-services.js
@@ -1,3 +1,4 @@
+
 const url = 'http://localhost:3000/products';
 
 
@@ -11,15 +12,25 @@ const createProduct = (name, price, img) => {
     return fetch(url, {
         method: 'POST',
         headers: {
-            'content-type': 'application/json',
+            'content-type': 'application/json'
         },
-        body: JSON.stringify({name, price, img})
+        body: JSON.stringify({ name, price, img })
     }).then((response) => response.json())
-    .catch((error) => console.log(error));
-}
+        .catch((error) => console.log(error));
+};
+
+
+const deleteProduct = (id) => {
+    return fetch((`http://localhost:3000/products/${id}`), {
+        method: 'DELETE',
+        header: {
+            'content-type': 'application/json'
+        }
+    }).then((response) => response.json()).catch((error) => console.log(error))
+};
 
 
 
 export const servicesProducts = {
-    getProducts, createProduct
+    getProducts, createProduct, deleteProduct
 };
